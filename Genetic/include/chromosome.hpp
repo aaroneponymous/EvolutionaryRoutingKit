10c7bf5251d2c6fb037c332120de9bb0bfa725f6
aaroneponymous <apaul9@u.rochester.edu>
2024-05-24 16:20:45 -0400

    Updating Genetic Template (More Exhaustive: Accepts Primitive and Non-Primitive Types) and operators == and !=

--- Genetic/include/chromosome.hpp
+++ Genetic/include/chromosome.hpp
@@ -6,7 +6,7 @@
 #ifndef CHROMOSOME_HPP
 #define CHROMOSOME_HPP
 
-
+#include <memory>
 
 namespace GENETIC {
 
@@ -28,16 +28,85 @@ namespace GENETIC {
     *           is being used to solve
    */
 
-  template<typename Container, typename... Params>
-  using CHR = Container<Params...>;
 
-  
+    // Trait to check if a type is a container
+    template<typename T, typename _ = void>
+    struct is_container : std::false_type {};
+
+    template<typename... Ts>
+    struct is_container_helper {};
+
+    template<typename T>
+    struct is_container<T, std::conditional_t<
+        false,
+        is_container_helper<
+            typename T::value_type,
+            typename T::size_type,
+            typename T::allocator_type,
+            typename T::iterator,
+            typename T::const_iterator,
+            decltype(std::declval<T>().size()),
+            decltype(std::declval<T>().begin()),
+            decltype(std::declval<T>().end())
+        >,
+        void
+    >> : std::true_type {};
 
 
 
+    // Primary template for a single parameter
+    template<typename Param>
+    struct CHR {
+        using type = Param;
+        std::unique_ptr<Param> value;
+
+        // Constructor
+        explicit CHR(const Param& param) : value(std::make_unique<Param>(param)) {}
+
+        // Copy constructor
+        CHR(const CHR& other) : value(std::make_unique<Param>(*other.value)) {}
+
+        // Move constructor
+        CHR(CHR&& other) noexcept : value(std::move(other.value)) {}
+
+        // Copy assignment operator
+        CHR& operator=(const CHR& other) {
+            if (this != &other) {
+                value = std::make_unique<Param>(*other.value);
+            }
+            return *this;
+        }
+
+        // Move assignment operator
+        CHR& operator=(CHR&& other) noexcept {
+            if (this != &other) {
+                value = std::move(other.value);
+            }
+            return *this;
+        }
+
+        // Destructor
+        ~CHR() = default;
+
+
+        // Equality Operator
+        bool operator==(const CHR& other) const {
+            if constexpr (is_container<Param>::value) {
+                return *value == *other.value;
+            }
+            else {
+                return *value == *other.value;
+            }
+        }
+
+        // Inequality operator
+        bool operator!=(const CHR& other) const {
+            return !(*this == other);
+        }
 
 
 
+    };
 
 }
 
---
+++ Genetic/src/main.cpp
@@ -0,0 +1,13 @@
+#include "chromosome.hpp"
+#include <tuple>
+#include <iostream>
+
+
+int main()
+{
+
+
+    GENETIC::CHR single_parameter(0);
+    
+
+}
