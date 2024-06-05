bf72143d0acd2bb8a5e4f3dea48e87199cd19119
aaroneponymous <apaul9@u.rochester.edu>
2024-06-05 18:23:12 -0400

    Change Chromosome Implementation: Merge the CHR struct into Chromosome Class (Avoid Redundancy))

--- Genetic/include/chromosome.hpp
+++ Genetic/include/chromosome.hpp
@@ -1,6 +1,7 @@
 /**
  * @file    chromosome.hpp
  * @brief   Contains Template for Chromosome used in GA
+ *
  */
 
 #ifndef CHROMOSOME_HPP
@@ -11,126 +12,118 @@
 #include <tuple>
 #include <type_traits>
 #include <iostream>
+#include <utility>
+
+/**
+ * @brief: 1. Variadic Template
+ *         2. Tuple for Gene Storage
+ *
+ */
+
+/**
+ * @typedef: Chromosome Struct: CHR
+ * @params:  1. Container: Specifies Generic Container for e.g. vector<T>, tuple<T>
+ *           2. Params:    Generic Parameter Pack
+ *
+ * @brief:   Allows for flexibility in terms of defining the Gene with a
+ *           Templated Parameter Pack (number and variation in the type
+ *           of parameters) based on the problem the Genetic Algorithm
+ *           is being used to solve
+ */
+
+// [x]: Incorporate SFINAE to detect and handle different container types
+
+// [ ]: User Type Objects Not Supported
 
 // Check if a type is a container
-template <typename T, typename _ = void>
-struct is_container : std::false_type
-{
-};
+template<typename T, typename _ = void>
+struct is_container : std::false_type {};
 
-template <typename... Ts>
-struct is_container_helper
-{
-};
+template<typename... Ts>
+struct is_container_helper {};
 
-template <typename T>
+template<typename T>
 struct is_container<T, std::conditional_t<
-                           false,
-                           is_container_helper<
-                               typename T::value_type,
-                               typename T::size_type,
-                               typename T::allocator_type,
-                               typename T::iterator,
-                               typename T::const_iterator,
-                               decltype(std::declval<T>().size()),
-                               decltype(std::declval<T>().begin()),
-                               decltype(std::declval<T>().end())>,
-                           void>> : std::true_type
-{
-};
+    false,
+    is_container_helper<
+        typename T::value_type,
+        typename T::size_type,
+        typename T::allocator_type,
+        typename T::iterator,
+        typename T::const_iterator,
+        decltype(std::declval<T>().size()),
+        decltype(std::declval<T>().begin()),
+        decltype(std::declval<T>().end())
+    >, void >> : std::true_type {};
 
 // Helper function to print tuple elements
-template <typename Tuple, std::size_t... Is>
-void print_tuple_impl(const Tuple &t, std::index_sequence<Is...>)
-{
+template<typename Tuple, std::size_t... Is>
+void print_tuple_impl(const Tuple& t, std::index_sequence<Is...>) {
     ((std::cout << (Is == 0 ? "" : ", ") << std::get<Is>(t)), ...) << std::endl;
 }
 
-template <typename... Args>
-void print_tuple(const std::tuple<Args...> &t)
-{
+template<typename... Args>
+void print_tuple(const std::tuple<Args...>& t) {
     print_tuple_impl(t, std::index_sequence_for<Args...>{});
 }
 
 // Check if a type is a tuple
-template <typename T>
-struct is_tuple : std::false_type
-{
-};
+template<typename T>
+struct is_tuple : std::false_type {};
 
-template <typename... Args>
-struct is_tuple<std::tuple<Args...>> : std::true_type
-{
-};
+template<typename... Args>
+struct is_tuple<std::tuple<Args...>> : std::true_type {};
 
-namespace GENETIC
-{
+namespace GENETIC {
 
-    /**
-     * @brief: 1. Variadic Template
-     *         2. Tuple for Gene Storage
-     *
-     */
+    template<typename Param, typename T>
+    class Chromosome {
+        static_assert(is_container<Param>::value || is_tuple<Param>::value, "Param must be a std::vector or std::tuple");
+        static_assert(std::is_arithmetic<T>::value, "T must be an arithmetic type");
+    private:
+        using type = Param;
+        Param genes_;                   // represents the estimated parameter(s)
+        Param result_;                  // result after application of chromosome objective function(s)
+        std::string encoded_chromo;     // string of bits representing chromosome
 
-    /**
-     * @typedef: Chromosome Struct: CHR
-     * @params:  1. Container: Specifies Generic Container for e.g. vector<T>, tuple<T>
-     *           2. Params:    Generic Parameter Pack
-     *
-     * @brief:   Allows for flexibility in terms of defining the Gene with a
-     *           Templated Parameter Pack (number and variation in the type
-     *           of parameters) based on the problem the Genetic Algorithm
-     *           is being used to solve
-     */
+        T sum_fitness_;                 // total sum of objective function(s) results
+        int chromo_size_;               // total no. of bits in encoded_chromo
+        int num_generation_;            // no. of generation
 
-    // [x]: Incorporate SFINAE to detect and handle different container types
+    public:
+        
 
-    // [ ]: User Type Objects Not Supported
 
-    // Helper function to print tuple elements
-    template <typename Tuple, std::size_t... Is>
-    void print_tuple_impl(const Tuple &t, std::index_sequence<Is...>)
-    {
-        ((std::cout << (Is == 0 ? "" : ", ") << std::get<Is>(t)), ...) << std::endl;
-    }
 
-    template <typename... Args>
-    void print_tuple(const std::tuple<Args...> &t)
-    {
-        print_tuple_impl(t, std::index_sequence_for<Args...>{});
-    }
+    };
+    
 
     // Primary template for a single parameter
-    template <typename Param>
-    struct CHR
-    {
+    template<typename Param>
+    struct CHR {
         using type = Param;
         std::unique_ptr<Param> value;
 
         // Constructor
-        explicit CHR(const Param &param) : value(std::make_unique<Param>(param)) {}
+        explicit CHR(const Param& param) : value(std::make_unique<Param>(param)) {}
 
         // Copy constructor
-        CHR(const CHR &other) : value(std::make_unique<Param>(*other.value)) {}
+        CHR(const CHR& other) : value(std::make_unique<Param>(*other.value)) {}
 
         // Move constructor
-        CHR(CHR &&other) noexcept : value(std::move(other.value)) {}
+        CHR(CHR&& other) noexcept : value(std::move(other.value)) {}
 
         // Copy assignment operator
-        CHR &operator=(const CHR &other)
-        {
-            if (this != &other)
-            {
+        CHR& operator=(const CHR& other) {
+            if (this != &other) {
                 value = std::make_unique<Param>(*other.value);
             }
             return *this;
         }
 
         // Move assignment operator
-        CHR &operator=(CHR &&other) noexcept
-        {
-            if (this != &other)
-            {
+        CHR& operator=(CHR&& other) noexcept {
+            if (this != &other) {
                 value = std::move(other.value);
             }
             return *this;
@@ -140,51 +133,40 @@ namespace GENETIC
         ~CHR() = default;
 
         // Equality operator
-        bool operator==(const CHR &other) const
-        {
+        bool operator==(const CHR& other) const {
             return *value == *other.value;
         }
 
         // Inequality operator
-        bool operator!=(const CHR &other) const
-        {
+        bool operator!=(const CHR& other) const {
             return !(*this == other);
         }
 
         // Print function for single parameter
-        void print() const
-        {
+        void print() const {
             print_impl(*value);
         }
 
     private:
         // Generic print implementation
-        template <typename T>
-        std::enable_if_t<!is_container<T>::value && !is_tuple<T>::value> print_impl(const T &param) const
-        {
+        template<typename T>
+        std::enable_if_t<!is_container<T>::value && !is_tuple<T>::value> print_impl(const T& param) const {
             std::cout << param << std::endl;
         }
 
-        template <typename T>
-        std::enable_if_t<is_container<T>::value && !is_tuple<T>::value> print_impl(const T &container) const
-        {
-            for (const auto &elem : container)
-            {
+        template<typename T>
+        std::enable_if_t<is_container<T>::value && !is_tuple<T>::value> print_impl(const T& container) const {
+            for (const auto& elem : container) {
                 std::cout << elem << " ";
             }
             std::cout << std::endl;
         }
 
-        template <typename T>
-        std::enable_if_t<is_tuple<T>::value> print_impl(const T &t) const
-        {
+        template<typename T>
+        std::enable_if_t<is_tuple<T>::value> print_impl(const T& t) const {
             print_tuple(t);
         }
     };
-
-    // Alias template for easier usage
-    template <typename Param>
-    using CHR_t = typename CHR<Param>::type;
 
 } // namespace GENETIC
 
--- Genetic/include/main.cpp
+++ Genetic/include/main.cpp
@@ -1,52 +1,69 @@
 #include "chromosome.hpp"
 #include <iostream>
-#include <tuple>
 #include <vector>
+#include <tuple>
 
+int main() {
+    // Create instances of CHR with integer parameters
+    GENETIC::CHR<int> chrSingle1(42);
+    GENETIC::CHR<int> chrSingle2(42);
+    GENETIC::CHR<int> chrSingle3(43);
 
+    // Print values
+    chrSingle1.print(); // Output: 42
+    chrSingle2.print(); // Output: 42
+    chrSingle3.print(); // Output: 43
 
-int main()
-{
-
-    // Instance of CHR with single parameters
-
-    GENETIC::CHR<int> single_int1(32);
-    GENETIC::CHR<int> single_int2(32);
-    GENETIC::CHR<int> single_int3(1);
-
-
-    single_int1.print();
-    single_int2.print();
-    single_int3.print();
-
-
-    // Instance of Tuple Multiple Param
-    auto tuple_1 = std::make_tuple(1, 2.0, 'a');
-    auto tuple_2 = std::make_tuple(1, 2.0, 'a');
-    auto tuple_3 = std::make_tuple(2, 3.0, 'a');
-
-    GENETIC::CHR<std::tuple<int, double, char>> multi_tuple1(tuple_1);
-    GENETIC::CHR<std::tuple<int, double, char>> multi_tuple2(tuple_2);
-    GENETIC::CHR<std::tuple<int, double, char>> multi_tuple3(tuple_3);
-
-
-    std::cout << "\nPrinting Tuple-Based Chromosome" << std::endl;
-    multi_tuple1.print();
-    multi_tuple2.print();
-    multi_tuple3.print();
-
-
-    std::cout << "\nTesting Operators for Tuple-Based Chromosome" << std::endl;
-    if (multi_tuple1 == multi_tuple2) {
-        std::cout << "Tuple 1 and Tuple 2 are equal" << std::endl;
+    // Compare instances
+    if (chrSingle1 == chrSingle2) {
+        std::cout << "chrSingle1 and chrSingle2 are equal." << std::endl; // This will be printed
     }
 
-    if (multi_tuple1 != multi_tuple3) {
-        std::cout << "Tuple 1 and Tuple 3 are not equal" << std::endl;
-
+    if (chrSingle1 != chrSingle3) {
+        std::cout << "chrSingle1 and chrSingle3 are not equal." << std::endl; // This will be printed
     }
 
+    // Create instances of CHR with vector parameters
+    GENETIC::CHR<std::vector<int>> chrVec1(std::vector<int>{1, 2, 3});
+    GENETIC::CHR<std::vector<int>> chrVec2(std::vector<int>{1, 2, 3});
+    GENETIC::CHR<std::vector<int>> chrVec3(std::vector<int>{1, 2, 4});
+
+    // Print values
+    chrVec1.print(); // Output: 1 2 3
+    chrVec2.print(); // Output: 1 2 3
+    chrVec3.print(); // Output: 1 2 4
+
+    // Compare instances
+    if (chrVec1 == chrVec2) {
+        std::cout << "chrVec1 and chrVec2 are equal." << std::endl; // This will be printed
+    }
+
+    if (chrVec1 != chrVec3) {
+        std::cout << "chrVec1 and chrVec3 are not equal." << std::endl; // This will be printed
+    }
+
+    // Create instances of CHR with tuple parameters
+    auto tuple1 = std::make_tuple(1, 2.0, 'a');
+    auto tuple2 = std::make_tuple(1, 2.0, 'a');
+    auto tuple3 = std::make_tuple(2, 3.0, 'b');
+
+    GENETIC::CHR<std::tuple<int, double, char>> chrTuple1(tuple1);
+    GENETIC::CHR<std::tuple<int, double, char>> chrTuple2(tuple2);
+    GENETIC::CHR<std::tuple<int, double, char>> chrTuple3(tuple3);
+
+    // Print values
+    chrTuple1.print(); // Output: 1, 2.0, a
+    chrTuple2.print(); // Output: 1, 2.0, a
+    chrTuple3.print(); // Output: 2, 3.0, b
+
+    // Compare instances
+    if (chrTuple1 == chrTuple2) {
+        std::cout << "chrTuple1 and chrTuple2 are equal." << std::endl; // This will be printed
+    }
+
+    if (chrTuple1 != chrTuple3) {
+        std::cout << "chrTuple1 and chrTuple3 are not equal." << std::endl; // This will be printed
+    }
 
     return 0;
-
 }
